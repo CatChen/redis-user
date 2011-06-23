8327f4b13d3aa311ab27d365575f3e4ec50a3263
Cat Chen <cathsfz@gmail.com>
2011-06-23 17:42:00 +0800

    changed user to User; changed role to Role

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const user = require('./user');
 const role = require('./role');
+const redis = require('redis');
 
-module.exports = {
-    user: user,
-    role: role
+module.exports = function(redisClient) {
+    return {
+        User: user(redisClient),
+        Role: role(redisClient)
+    };
 };
