a89d27da264fc23c0f9904c2e6e044a41277315b
Cat Chen <cathsfz@gmail.com>
2011-06-23 17:42:21 +0800

    created interface for user and role

--- role.js
+++ role.js
@@ -1,3 +1,12 @@
-module.exports = (function() {
-    
-})();
+module.exports = function(redisClient) {
+    return {
+        createRole: function(name) {},
+        getRole: function(name) {},
+        getAllRoles: function() {},
+        deleteRole: function(name) {},
+        addUsersToRoles: function(userEmails, roleNames) {},
+        removeUsersFromRoles: function(userEmails, roleNames) {},
+        getRolesForUser: function(userEmail) {},
+        isUserInRole: function(userEmail, roleName) {}
+    };
+};
--- user.js
+++ user.js
@@ -1,3 +1,14 @@
-module.exports = (function() {
+const crypto = require('crypto');
+const sha1 = crypto.createHash('sha1');
+
+module.exports = function(redisClient) {
+    var computeSHA1 = function(str) { return sha1.update(str).digest('hex'); };
     
-})();
+    return {
+        createUser: function(email, password) {},
+        getUser: function(email) {},
+        getAllUsers: function() {},
+        updateUserPassword: function(email, oldPassword, newPassword) {},
+        deleteUser: function(email) {}
+    };
+};
