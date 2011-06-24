0d25074890dc75234f36afdbd80a15b0d48c29e7
Cat Chen <cathsfz@gmail.com>
2011-06-24 08:23:33 +0800

    working copy

--- user.js
+++ user.js
@@ -5,12 +5,32 @@ module.exports = function(redisClient) {
     var computeSHA1 = function(str) { return sha1.update(str).digest('hex'); };
     
     return {
-        createUser: function(email, password) {
-            var id = redis.incr('global:nextUserId');
+        createUser: function(email, password, callback) {
+            redis.incr('global:nextUserId', function(error, id) {
+                if (error) {
+                    callback(false);
+                } else {
+                    redis.multi()
+                        .set('user:' + id + ':email', email)
+                        .set('user:' + id + ':password', computeSHA1(password))
+                        .set('user:' + email + ':id', id)
+                        .exec(function(error, results) {
+                            if (error) {
+                                callback(false);
+                            } else {
+                                callback(true);
+                            }
+                        });
+                }
+            });
         },
-        getUser: function(email) {},
-        getAllUsers: function() {},
-        updateUserPassword: function(email, oldPassword, newPassword) {},
-        deleteUser: function(email) {}
+        getUser: function(email, callback) {
+            redis.get('user:' + email + ':id', function(error, id) {
+                
+            });
+        },
+        getAllUsers: function(callback) {},
+        updateUserPassword: function(email, oldPassword, newPassword, callback) {},
+        deleteUser: function(email, callback) {}
     };
 };
