be17fd52a51298c28bc278637d1c544bbc2eaec1
Cat Chen <cathsfz@gmail.com>
2011-07-19 16:41:25 +0800

    added Async facade

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const user = require('./user');
-const role = require('./role');
+const Async = require('jshelpers').Async;
 
-module.exports = function(redisClient) {
-    var result = user(redisClient);
-    result.role = role(redisClient, result);
+var addAsyncFacade = function(obj) {
+    for (var key in obj) {
+        (function(key) {
+            var item = obj[key];
+            if (item instanceof Function) {
+                obj[key + 'Async'] = function() {
+                    var operation = new Async.Operation();
+                    var argumentsArray = Array.prototype.slice.call(arguments, 0);
+                    var callback = function(result) { operation.yield(result); };
+                    argumentsArray.push(callback);
+                    console.log('calling ' + key);
+                    console.log('arguments: ' + argumentsArray);
+                    obj[key].apply(obj, argumentsArray);
+                    return operation;
+                };
+            }
+        })(key);
+    }
+    return obj;
+};
+
+module.exports = function(redisClient, useAsync) {
+    const user = require('./user')(redisClient);
+    const role = require('./role')(redisClient, user);
+    
+    var result;
+    if (useAsync) {
+        result = addAsyncFacade(user);
+        result.role = addAsyncFacade(role);
+    } else {
+        result = user;
+        result.role = role;
+    }
     return result;
 };
